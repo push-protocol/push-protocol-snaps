7d02f07b9a710668b75efc6791c40c6b295cc360
Arch0125 <dasarchisman25@gmail.com>
2023-12-01 16:37:33 +0530

    add: remove auto snooze

--- snap/src/index.ts
+++ snap/src/index.ts
@@ -374,6 +374,33 @@ export const onCronjob: OnCronjobHandler = async ({ request }) => {
       }
       break;
     }
+    case 'pushproto_removesnooze':{
+      const persistedData = await SnapStorageCheck();
+      const snoozeFlag = persistedData.popuptoggle;
+      if(Number(snoozeFlag)>=41){
+        const data = {
+          addresses: persistedData.addresses,
+          popuptoggle: 0,
+        };
+  
+        await snap.request({
+          method: "snap_manageState",
+          params: { operation: "update", newState: data },
+        });
+
+        await snap.request({
+          method:"snap_dialog",
+          params:{
+            type:"alert",
+            content:panel([
+              heading("Snooze Alert"),
+              text("Notification snooze has been turned off, you will start getting popup notifications from now on.")
+            ])
+          }
+        })
+      }
+      break;
+    }
     default:
       throw new Error("Method not found.");
   }
